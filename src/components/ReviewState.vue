e64667f9cb97b0c446c14b5cca7a07d5916e532b
welang <7542570+welang2020@user.noreply.gitee.com>
2020-10-30 14:26:04 +0800

    1.对manifest做处理

--- src/components/ReviewState.vue
+++ src/components/ReviewState.vue
@@ -16,81 +16,12 @@ import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 @Component({})
 export default class ReviewState extends Vue {
     @Prop({default: ''}) status!: any; // 状态
-    stateText = '';  // 状态文本
-    hintText = '';  // 校验文本
-    type = '';  // 图标类型
+    @Prop({default: ''}) stateText!:string;  // 状态文本
+    @Prop({default: ''}) hintText!:string;  // 校验文本
+    @Prop({default: ''}) type!: string;  // 图标类型
 
     created () {
-           console.log(this.status);
-           switch (this.status) {
-                case 22:
-                    this.type = 'wait';
-                    this.stateText = '测评作业待审核';
-                    this.hintText = '作业将在两天内出审核结果';
-                    break;
-                case 15:
-                    this.type = 'wait';
-                    this.stateText = '订单待审核';
-                    this.hintText = '订单将在两天内出审核结果';
-                    break;
-                case 1:
-                     this.type = 'wait';
-                     this.stateText = '等待审核资格';
-                     this.hintText = '资格将在两天内出审核结果';
-                     break;
-                 case 40: 
-                    this.type = 'wait';
-                    this.stateText = '作业待审核';
-                    this.hintText = '作业将在两天内出审核结果';
-                    break;
-                case 23:
-                    this.type = 'error';
-                    this.stateText = '测评作业审核未通过';
-                    this.hintText = '请重新上传测评作业'
-                    break;
-
-                case 17:
-                    this.type = 'error';
-                    this.stateText = '订单审核未通过';
-                    this.hintText = '请重新上传订单';
-                    break;
-                case 50:
-                    this.type = 'error';
-                    this.stateText = '作业审核未通过';
-                    this.hintText = '请重新提交作业';
-                    break;
-                case 10:
-                    this.type = 'success';
-                    this.stateText = '资格审核通过';
-                    this.hintText = '请去提交订单';
-                    break;
-                case 30:
-                    this.type = 'success';
-                    this.stateText = '订单审核通过';
-                    this.hintText = '请去提交作业';
-                    break;
-                case 21:
-                    this.type = 'success';
-                    this.stateText = '资格审核通过';
-                    this.hintText = '请去提交测评作业';
-                    break;
-                case 60:
-                    this.type = 'success';
-                    this.stateText = '作业审核通过';
-                    this.hintText = '通过审核，作业完成，作业奖励已发放至您的账户余额';
-                    break;
-                case 90:
-                    this.type = 'finish';
-                    this.stateText = '恭喜您，已成功完成任务!';
-                    this.hintText = '奖励金已转入您的余额，请注意查收~';
-                    break;
-                case -1 || -2 || -3 || -4 || -5:
-                    this.type = 'finish';
-                    this.stateText = '抱歉，任务已取消!';
-                    this.hintText = '商家拒绝了您的任务申请，换别的任务试试吧~';
-                    break;
-            }  
-         
+ 
     }
 }
 
